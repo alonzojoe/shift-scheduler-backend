b1ae6fb3d10b9cbb3fa3a37ed993da1e59924316
alonzojoe <alonzojoenell2k15@gmail.com>
2025-06-05 09:18:07 +0800

    app with express with a port of 3000 and create  sample route api end point /test

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("shift scheduler api");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/test", (_, res) => {
+  res.send({
+    message: "Hello World",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
